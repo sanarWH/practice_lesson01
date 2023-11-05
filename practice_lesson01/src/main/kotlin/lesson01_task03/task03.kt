b2bee07c68d9db6ccb43f61b0cd660858606c9ed
sanarWH <vkhv0702@gmail.com>
2023-11-05 20:28:09 +0300

    KS-1-3 solution

---
+++ practice_lesson01/src/main/kotlin/lesson01_task03/task03.kt
@@ -0,0 +1,14 @@
+package lesson01_task03
+
+fun main() {
+
+    val year = 1961
+    var hour = 9
+    var minute = 7
+    hour = 10
+    minute = 55
+
+    println("Год полета Юрия Гагарина в космос: $year")
+    print("Время посадки Юрия Гагарина на землю $hour:")
+    println("$minute")
+}
