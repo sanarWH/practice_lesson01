790f23c56b8182f01838d1916e6aed0f0bf674c4
sanarWH <vkhv0702@gmail.com>
2023-11-06 19:28:49 +0300

    KS-1-5 solution

--- practice_lesson01/src/main/kotlin/lesson01_task03/task03.kt
+++ practice_lesson01/src/main/kotlin/lesson01_task03/task03.kt
@@ -3,10 +3,10 @@ package lesson01_task03
 fun main() {
 
     val year = 1961
-    var hour = 9
-    var minute = 7
-    hour = 10
-    minute = 55
+    var hour: String = "09"
+    var minute: String = "07"
+    hour = "10"
+    minute = "55"
 
     println("Год полета Юрия Гагарина в космос: $year")
     print("Время посадки Юрия Гагарина на землю $hour:")
